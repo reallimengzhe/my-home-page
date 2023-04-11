124af608ad5aa93aee9944b612e6918da3dda169
li.mengzhe <li.mengzhe@outlook.com>
2023-04-12 00:34:11 +0800

    initial

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,6 +4,7 @@ import '@/styles/theme-switcher.css'
 
 import '@arco-design/web-react/dist/css/arco.css'
 
+import Head from 'next/head'
 import NavBar from '@/components/nav-bar'
 import LayoutHeader from '@/components/layout-header'
 import LayoutFooter from '@/components/layout-footer'
@@ -11,6 +12,14 @@ import LayoutFooter from '@/components/layout-footer'
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>李孟哲</title>
+        <meta name="description" content="李孟哲的个人博客" />
+        <meta
+          name="keywords"
+          content="李孟哲, li mengzhe, limengzhe, 前端开发, Web develop"
+        />
+      </Head>
       <NavBar />
       <LayoutHeader />
       <Component {...pageProps} />
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,14 +3,8 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="zh-CN">
-      <Head>
-        <title>李孟哲</title>
-        <meta name="description" content="李孟哲的个人博客" />
-        <meta
-          name="keywords"
-          content="李孟哲, li mengzhe, limengzhe, 前端开发, Web develop"
-        />
-      </Head>
+      <Head />
+
       <body className="text-sm">
         <Main />
         <NextScript />
